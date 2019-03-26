bbf0b45c9fa22c967e218185a5cf58b8d4b7e8b8
Pankaj Shinde2 <PS00475391@TechMahindra.com>
2019-03-26 23:12:07 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatListModule, MatIconModule } from '@angular/material';
+import { CustomersComponent } from './customers/customers/customers.component';
+import { CustomerHeaderComponent } from './customers/customers/customer-header/customer-header.component';
+import { CustomerToolbarComponent } from './customers/customers/customer-toolbar/customer-toolbar.component';
+import { CustomerListComponent } from './customers/customers/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CustomersComponent,
+    CustomerHeaderComponent,
+    CustomerToolbarComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/customers/customers/customer-toolbar/customer-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-toolbar',
+  templateUrl: './customer-toolbar.component.html',
+  styleUrls: ['./customer-toolbar.component.less']
+})
+export class CustomerToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
